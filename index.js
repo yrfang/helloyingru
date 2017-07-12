c4a24a7f0e06e6e6fc628c6048a1afe81c36dc61
yrfang <fyr1116@gmail.com>
2017-07-12 19:39:16 +0800

    resume-icon-rwd

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const menuToggle = document.getElementById("menu_toggle");
 const toggle = document.getElementById("toggle");
 const sideNav = document.getElementById("side-nav");
+const content = document.getElementById("content");
 
 menuToggle.addEventListener('click', function(e){
-  if (e.target.classList.contains('fa-bars')) {
+  if (toggle.classList.contains('fa-bars')) {
     // sideNav.classList.toggle('open');
     toggle.classList.remove('fa-bars');
     toggle.classList.add('fa-times');
+    content.style.width = "100vh - 210px";
+    content.style.transform = "translateX(210px)";
   } else {
     toggle.classList.remove('fa-times');
     toggle.classList.add('fa-bars');
+    content.style.width = "";
+    content.style.transform = "";
   }
 
   sideNav.classList.toggle('open');
