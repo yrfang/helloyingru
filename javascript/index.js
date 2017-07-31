81519f62573dd7ddc195ed9570ff9251b26f944d
yrfang <fyr1116@gmail.com>
2017-07-31 11:58:01 +0800

    document.ready

--- javascript/index.js
+++ javascript/index.js
@@ -1,46 +1,34 @@
-const menuToggle = document.getElementById("menu_toggle");
-const toggle = document.getElementById("toggle");
-const sideNav = document.getElementById("side-nav");
-const content = document.getElementById("content");
+$(document).ready(function() {
 
-// menuToggle.addEventListener('click', function(e){
-//   if (toggle.classList.contains('fa-bars')) {
-//     // sideNav.classList.toggle('open');
-//     toggle.classList.remove('fa-bars');
-//     toggle.classList.add('fa-times');
-//     content.style.width = "100vh - 210px";
-//     content.style.transform = "translateX(210px)";
-//   } else {
-//     toggle.classList.remove('fa-times');
-//     toggle.classList.add('fa-bars');
-//     content.style.width = "";
-//     content.style.transform = "";
-//   }
-//
-//   sideNav.classList.toggle('open');
-// });
+  const menuToggle = document.getElementById("menu_toggle");
+  const toggle = document.getElementById("toggle");
+  const sideNav = document.getElementById("side-nav");
+  const content = document.getElementById("content");
 
-function menuToogle(e) {
-  if (toggle.classList.contains('fa-bars')) {
-    // sideNav.classList.toggle('open');
-    toggle.classList.remove('fa-bars');
-    toggle.classList.add('fa-times');
-    content.style.width = "100vh - 210px";
-    content.style.transform = "translateX(210px)";
-  } else {
-    toggle.classList.remove('fa-times');
-    toggle.classList.add('fa-bars');
-    content.style.width = "";
-    content.style.transform = "";
-  }
+  menuToggle.addEventListener('click', function(e){
+    if (toggle.classList.contains('fa-bars')) {
+      // sideNav.classList.toggle('open');
+      toggle.classList.remove('fa-bars');
+      toggle.classList.add('fa-times');
+      content.style.width = "100vh - 210px";
+      content.style.transform = "translateX(210px)";
+    } else {
+      toggle.classList.remove('fa-times');
+      toggle.classList.add('fa-bars');
+      content.style.width = "";
+      content.style.transform = "";
+    }
 
-  sideNav.classList.toggle('open');
-}
+    sideNav.classList.toggle('open');
+  });
 
 
-$(document).on('click', '#side-nav a', function(event){
-    event.preventDefault();
-    $('html, body').animate({
-        scrollTop: $( $.attr(this, 'href') ).offset().top
-    }, 500);
+  $(document).on('click', '#side-nav a', function(event){
+      event.preventDefault();
+      $('html, body').animate({
+          scrollTop: $( $.attr(this, 'href') ).offset().top
+      }, 500);
+  });
+
+
 });
