a927e26eae14e428a2ca481568f57d0c9623c652
yrfang <fyr1116@gmail.com>
2017-07-31 11:48:09 +0800

    toggle-function test

--- javascript/index.js
+++ javascript/index.js
@@ -3,7 +3,24 @@ const toggle = document.getElementById("toggle");
 const sideNav = document.getElementById("side-nav");
 const content = document.getElementById("content");
 
-menuToggle.addEventListener('click', function(e){
+// menuToggle.addEventListener('click', function(e){
+//   if (toggle.classList.contains('fa-bars')) {
+//     // sideNav.classList.toggle('open');
+//     toggle.classList.remove('fa-bars');
+//     toggle.classList.add('fa-times');
+//     content.style.width = "100vh - 210px";
+//     content.style.transform = "translateX(210px)";
+//   } else {
+//     toggle.classList.remove('fa-times');
+//     toggle.classList.add('fa-bars');
+//     content.style.width = "";
+//     content.style.transform = "";
+//   }
+//
+//   sideNav.classList.toggle('open');
+// });
+
+function menuToogle(e) {
   if (toggle.classList.contains('fa-bars')) {
     // sideNav.classList.toggle('open');
     toggle.classList.remove('fa-bars');
@@ -18,7 +35,7 @@ menuToggle.addEventListener('click', function(e){
   }
 
   sideNav.classList.toggle('open');
-});
+}
 
 
 $(document).on('click', '#side-nav a', function(event){
